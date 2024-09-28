44fe9b88ab565927c8002c3d49ce101554eecbc3
Tanmay <chiragmaheshwari781@gmail.com>
2024-09-28 20:26:37 +0530

    ADDING DEPARTMENT LIST EDIT FEATURE

--- Folder_Structure.py
+++ Folder_Structure.py
@@ -17,52 +17,89 @@ show_type = input("Enter the show type: ")
 show_dir = os.path.join(show_dir, show_name)
 os.mkdir(show_dir)
 
-
 print("These are the default directories for the show.")
+
 serial_number = 1
+
 department_list = functions.get_departments()
 functions.print_department_structure(department_list)
 
-department_edit = input("Do you want to edit the department list(Yes/No): ")
-department_edit = department_edit.lower()
+department_creation_decision = input("Do you want to go with Default Department or want to edit the Department Structure(Yes/No): ").lower()
+department_creation_decision = department_creation_decision.strip()
 
-if department_edit == "no":
-    for department_name in get_directory_list():
-        department_dir = os.path.join(show_dir, department_name)
-        os.mkdir(department_dir)
+if department_creation_decision == "yes":
 
-
-elif department_edit == "yes":
-    department_list = functions.get_departments()
+    while True:
+        user_action = (input("Do you to 'add','show','remove',edit' or 'exit':")).lower()
+        user_action = user_action.strip()
 
 
-    #THIS PRINTS ALL DEPARTMENTS
-    for index, department in enumerate(department_list):
-
-        if type(department) == dict:
-
-            for department_name in department.keys():
-                print(f"{index + 1}.{department_name.capitalize()}")
-
-            for department_parts in department.values():
-
-                for serial, department_part_names in enumerate(department_parts):
-                    print(f"  {serial + 1}.{department_part_names}")
-
-                print()
+        if user_action.startswith('add'):
+            department = user_action[4:]
+            department = department.capitalize()
 
 
-        elif type(department) == str:
-            print(f"{index + 1}.{department.capitalize()}\n")
+            department_list = functions.get_departments()
+
+            department_list.appen
+
+            write_todos(todo_List)
+
+
+        elif user_action.startswith('show'):
+
+            todo_List = get_todos()
+
+            todo_new_list = [item.strip('\n') for item in todo_List]
+            todo_List = todo_new_list
+
+
+            for index,x in enumerate(todo_List):
+                print(f"{index + 1}. {x.capitalize()}")
+
+
+        elif user_action.startswith('complete'):
+
+            try:
+                todo_List = get_todos()
+
+                completed_task = int(user_action.strip('complete '))
+                completed_task_value = todo_List[completed_task - 1].replace('\n','')
+                print(f'{completed_task_value.capitalize()} has been marked completed.')
+                todo_List.pop(completed_task-1)
+
+                write_todos(todo_List)
+
+            except IndexError:
+                print("Item is not in the list.")
+                continue
+
+
+        elif user_action.startswith('edit'):
+            try:
+                todo_List = get_todos()
+
+                edited_task = int(user_action[5:])
+                edited_task_value = todo_List[edited_task - 1].replace('\n','')
+                new_task = input("Enter the new task:")
+
+                print(f'{edited_task_value.capitalize()} has been changed to {new_task.capitalize()}.')
+                todo_List[edited_task- 1] = new_task.capitalize() + '\n'
+
+                write_todos(todo_List)
+
+
+            except ValueError:
+                print("Your command is invalid.")
+                continue
+
+        elif user_action.startswith("exit"):
+            break
 
 
         else:
-            pass
-
-#     department_list_action = input("What do you want to do(Add/Remove/Edit/Complete): ")
-#     if
+            print("Invalid command!")
 
 
-
-
+    print("Happy Comping!")
 
--- department_list_creator.py
+++ department_list_creator.py
@@ -1,3 +1,5 @@
+from modules import functions
+
 department_creation_decision = input("Do you want to go with Default Department or want to edit the Department Structure(Yes/No): ").lower()
 department_creation_decision = department_creation_decision.strip()
 
@@ -12,66 +14,90 @@ if department_creation_decision == "yes":
             department = user_action[4:]
             department = department.capitalize()
 
-            todo_List = get_todos()
-            todo_List.append(todo)
 
-            write_todos(todo_List)
+            additional_subfolders_decision = (input("Do you want to have sub-folders in this department(Yes/No): ")).lower()
+
+            if additional_subfolders_decision == "yes":
+                department_subfolder_list = []
+                while True:
+                    department_subfolders_name = input("Enter sub-folder name(One at a time): ")
+                    department_subfolder_list.append(department_subfolders_name)
+
+                    more_folder_decision = (input("Do you want to have more folders(Yes/No): ")).lower()
+
+                    if more_folder_decision == "yes":
+                        continue
+
+                    else:
+                        break
+
+                department = {department:department_subfolder_list}
+
+            else:
+                pass
+
+            department_list = functions.get_departments()
+            print(department_list)
+            department_list.append(department)
+            print(department_list)
+            department_list_str = str(department_list)
+
+
+
+            with open("appReq/departments_temp.json",'w') as file:
+                file.write(department_list_str)
+
 
 
         elif user_action.startswith('show'):
+            department_list = functions.get_departments()
+            functions.print_department_structure(department_list)
 
-            todo_List = get_todos()
-
-            todo_new_list = [item.strip('\n') for item in todo_List]
-            todo_List = todo_new_list
-
-
-            for index,x in enumerate(todo_List):
-                print(f"{index + 1}. {x.capitalize()}")
-
-
-        elif user_action.startswith('complete'):
-
-            try:
-                todo_List = get_todos()
-
-                completed_task = int(user_action.strip('complete '))
-                completed_task_value = todo_List[completed_task - 1].replace('\n','')
-                print(f'{completed_task_value.capitalize()} has been marked completed.')
-                todo_List.pop(completed_task-1)
-
-                write_todos(todo_List)
-
-            except IndexError:
-                print("Item is not in the list.")
-                continue
-
-
-        elif user_action.startswith('edit'):
-            try:
-                todo_List = get_todos()
-
-                edited_task = int(user_action[5:])
-                edited_task_value = todo_List[edited_task - 1].replace('\n','')
-                new_task = input("Enter the new task:")
-
-                print(f'{edited_task_value.capitalize()} has been changed to {new_task.capitalize()}.')
-                todo_List[edited_task- 1] = new_task.capitalize() + '\n'
-
-                write_todos(todo_List)
-
-
-            except ValueError:
-                print("Your command is invalid.")
-                continue
-
-        elif user_action.startswith("exit"):
-            break
-
-
-        else:
-            print("Invalid command!")
-
-
-    print("Happy Comping!")
-
+        #WAS FIGURING OUT JSON WRITING
+    #
+    #
+    #     elif user_action.startswith('complete'):
+    #
+    #         try:
+    #             todo_List = get_todos()
+    #
+    #             completed_task = int(user_action.strip('complete '))
+    #             completed_task_value = todo_List[completed_task - 1].replace('\n','')
+    #             print(f'{completed_task_value.capitalize()} has been marked completed.')
+    #             todo_List.pop(completed_task-1)
+    #
+    #             write_todos(todo_List)
+    #
+    #         except IndexError:
+    #             print("Item is not in the list.")
+    #             continue
+    #
+    #
+    #     elif user_action.startswith('edit'):
+    #         try:
+    #             todo_List = get_todos()
+    #
+    #             edited_task = int(user_action[5:])
+    #             edited_task_value = todo_List[edited_task - 1].replace('\n','')
+    #             new_task = input("Enter the new task:")
+    #
+    #             print(f'{edited_task_value.capitalize()} has been changed to {new_task.capitalize()}.')
+    #             todo_List[edited_task- 1] = new_task.capitalize() + '\n'
+    #
+    #             write_todos(todo_List)
+    #
+    #
+    #         except ValueError:
+    #             print("Your command is invalid.")
+    #             continue
+    #
+    #     elif user_action.startswith("exit"):
+    #         break
+    #
+    #
+    #     else:
+    #         print("Invalid command!")
+    #
+    #
+    # print("Happy Comping!")
+    #
