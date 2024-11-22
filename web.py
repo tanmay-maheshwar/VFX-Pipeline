04d906381f91b5a19902f71fb5d0edf8077e68f3
tanmay.maheshwari <chiragbabrala123@gmail.com>
2024-11-22 16:18:37 +0530

    Created Web App SHOTLIGHT

--- web.py
+++ web.py
@@ -1,8 +1,28 @@
 import streamlit as st
 import pandas as pd
 
+
+st.set_page_config(layout='wide')
 st.title("SHOTLIGHT")
 
-df = pd.DataFrame([1,2,3,4,5,6,7,8,9,10]
-             ,columns=["Shot_Number","Task"])
-st.dataframe(df)
+df = pd.read_excel('./Excels/data.xlsx','Sheet1',index_col =None)
+
+st.data_editor(df,
+             column_config={'#':st.column_config.Column(width='small',disabled=True),
+                            'SHOT_NUMBER':st.column_config.Column(disabled=True),
+                            'TASK':st.column_config.Column(disabled=True),
+                            'STATUS':st.column_config.Column(disabled=True),
+                            'THUMBNAIL':st.column_config.ImageColumn(),
+                            'DEPARTMENT':st.column_config.Column(disabled=True),
+                            'ARTIST':st.column_config.Column(disabled=True),
+                            'START DATE':st.column_config.Column(disabled=True),
+                            'ETA':st.column_config.Column(disabled=True),
+                            'NOTES':st.column_config.Column(disabled=True,width='large')
+                            },
+
+               hide_index=True,
+
+              )
+
+
+
