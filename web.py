97d77af43fe9d62f1f269cd349d0d993655d0eaa
tanmay.maheshwari <chiragbabrala123@gmail.com>
2024-11-26 20:49:01 +0530

    Adding Custom Interactable table

--- web.py
+++ web.py
@@ -5,24 +5,33 @@ import pandas as pd
 st.set_page_config(layout='wide')
 st.title("SHOTLIGHT")
 
-df = pd.read_excel('./Excels/data.xlsx','Sheet1',index_col =None)
+df = pd.read_excel('./Excels/data_01.xlsx','Sheet1',index_col =None)
+print(df['ARTIST'][0])
 
-st.data_editor(df,
-             column_config={'#':st.column_config.Column(width='small',disabled=True),
-                            'SHOT_NUMBER':st.column_config.Column(disabled=True),
-                            'TASK':st.column_config.Column(disabled=True),
+st.data_editor(df,key='my_data',
+             column_config={'Sno.':st.column_config.Column(width='small',disabled=True),
+                            'SHOT NAME':st.column_config.Column(disabled=True),
+                            'TASK':st.column_config.Column(width='large',disabled=True),
                             'STATUS':st.column_config.Column(disabled=True),
                             'THUMBNAIL':st.column_config.ImageColumn(),
                             'DEPARTMENT':st.column_config.Column(disabled=True),
-                            'ARTIST':st.column_config.Column(disabled=True),
+                            'ARTIST':st.column_config.Column(),
+                            'VERSION':st.column_config.Column(disabled=True),
                             'START DATE':st.column_config.Column(disabled=True),
                             'ETA':st.column_config.Column(disabled=True),
                             'NOTES':st.column_config.Column(disabled=True,width='large')
                             },
 
                hide_index=True,
+               height=700
 
               )
 
 
+data = st.session_state['my_data']['edited_rows']
+data_list = list(data)
 
+try:
+    print(data_list)
+except IndexError:
+    pass
